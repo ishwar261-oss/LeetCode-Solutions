7beeb5bdb9eb385c2261aabdf4564db1e34b3146
ISHWARz <ishwaranpat261@gmail.com>
2026-07-01 12:02:26 +0530

    Refactor myPow function and main for clarity

--- Array/Pow(x, n)/Solution.cpp
+++ Array/Pow(x, n)/Solution.cpp
@@ -2,29 +2,20 @@
 using namespace std;
 
 double myPow(double x, int n) {
-
     long long power = n;
     double ans = 1.0;
-
     if (power < 0) {
         x = 1 / x;
         power = -power;
     }
-
     while (power > 0) {
-
-        if (power % 2 == 1)
-            ans *= x;
-
+        if (power % 2 == 1) ans *= x;
         x *= x;
         power /= 2;
     }
-
     return ans;
 }
-
 int main(){
-
     double x;
     int n;
     
@@ -34,6 +25,5 @@ int main(){
     cin>>n;
 
     cout<<"The "<<n<<"th power of "<<x<<" is : "<<myPow(x,n)<<endl;
-    
     return 0;
 }
