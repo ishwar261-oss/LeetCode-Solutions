66fcd239622f24f7868849fb0d44173644393678
ISHWARz <ishwaranpat261@gmail.com>
2026-07-18 21:12:46 +0530

    Add files via upload

---
+++ Bit Manipulation/476. Number Complement/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findComplement(int num) {
+
+        long long mask = 1;
+
+        while (mask <= num) {
+            mask <<= 1;
+        }
+
+        return num ^ (mask - 1);
+    }
+};
